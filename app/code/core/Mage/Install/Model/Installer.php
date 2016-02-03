b82d471229d8bb19e8640659b4a3a211eb0963c7
SaurabhMehta007 <saurabh.hunt007@gmail.com>
2016-02-03 11:11:34 +0530

    Revert "Fix/featured cakes"

--- app/code/core/Mage/Install/Model/Installer.php
+++ app/code/core/Mage/Install/Model/Installer.php
@@ -10,18 +10,18 @@
  * http://opensource.org/licenses/osl-3.0.php
  * If you did not receive a copy of the license and are unable to
  * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
+ * to license@magento.com so we can send you a copy immediately.
  *
  * DISCLAIMER
  *
  * Do not edit or add to this file if you wish to upgrade Magento to newer
  * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
+ * needs please refer to http://www.magento.com for more information.
  *
  * @category    Mage
  * @package     Mage_Install
- * @copyright   Copyright (c) 2014 Magento Inc. (http://www.magentocommerce.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 
@@ -140,18 +140,16 @@ class Mage_Install_Model_Installer extends Varien_Object
      * @return  Mage_Install_Model_Installer
      */
     public function installConfig($data)
-		{
-			$data['db_active'] = true;
+    {
+        $data['db_active'] = true;
 
-			$data = Mage::getSingleton('install/installer_db')->checkDbConnectionData($data);
-			//Added by MagenTech.Com : to install sample DB for quickstart package
-			Mage::getSingleton('install/installer_Sample')->installSampleDB($data);
-			//end added by MagenTech.Com 
-			Mage::getSingleton('install/installer_config')
-				->setConfigData($data)
-				->install();
-			return $this;
-		}
+        $data = Mage::getSingleton('install/installer_db')->checkDbConnectionData($data);
+
+        Mage::getSingleton('install/installer_config')
+            ->setConfigData($data)
+            ->install();
+        return $this;
+    }
 
     /**
      * Database installation
